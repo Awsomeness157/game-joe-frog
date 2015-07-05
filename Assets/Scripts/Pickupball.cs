3f75db9f1497ce8611e6f93f0ba6eafa8658f274
Awsomeness157 <matthewhaacke@gmail.com>
2015-07-05 15:28:54 -0400

    First Commit
    I created my first commit

---
+++ Assets/Scripts/Pickupball.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickupball : MonoBehaviour {
+
+	Transform mytransform;
+	public Transform handTransform;
+	public bool isHolding;
+	public Transform ball;
+	public Rigidbody ballBody;
+
+	// Use this for initialization
+	void Start () {
+		mytransform = transform;
+		isHolding = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 handPos = handTransform.position;
+
+		if(!isHolding && Input.GetKeyDown (KeyCode.Q)){
+		isHolding = true;
+		
+		}
+
+		if(isHolding) {
+			ball.transform.position = handPos;
+			ballBody.useGravity = false;
+			ballBody.isKinematic = true;
+			ballBody.mass = 0;
+
+			if(Input.GetKeyDown(KeyCode.Return)) {
+				isHolding = false;
+
+			}
+		}
+
+		if(!isHolding){
+
+			ballBody.mass = 1;
+			ballBody.useGravity = true;
+			ballBody.isKinematic = false;
+
+		}
+
+	}
+}
