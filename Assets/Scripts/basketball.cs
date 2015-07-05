4650ced0712bfe602f6b33646b38c5f0475e8808
Awsomeness157 <matthewhaacke@gmail.com>
2015-07-05 16:19:39 -0400

    Added sound to bbal

---
+++ Assets/Scripts/basketball.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basketball : MonoBehaviour {
+
+
+	public AudioClip boink;
+	public AudioSource myAudio;
+	public static int score;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		}
+void OnCollisionEnter(Collision colider){
+	score++;
+	myAudio.PlayOneShot(boink);
+	Debug.Log (score);
+}
+
+}
+
+
