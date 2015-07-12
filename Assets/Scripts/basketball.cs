03369d6703a2e2558aad3a161c1051148b3a4ff3
Awsomeness157 <matthewhaacke@gmail.com>
2015-07-12 18:11:10 -0400

    added curry and sound to sped for bball

--- Assets/Scripts/basketball.cs
+++ Assets/Scripts/basketball.cs
@@ -15,11 +15,24 @@ public class basketball : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		}
-void OnCollisionEnter(Collision colider){
-	score++;
-	myAudio.PlayOneShot(boink);
-	Debug.Log (score);
-}
+
+		
+
+void OnCollisionEnter(Collision collider){
+	
+	float bBallVolume = GetComponent<Rigidbody>().velocity.magnitude/ 40;
+	//Debug.Log ("bBallVolume: " + bBallVolume);
+	myAudio.PlayOneShot(boink, bBallVolume);
+	//Debug.Log (score);
+	if(collider.gameObject.tag == "head"){
+		score++;
+		Debug.Log ("score:" + score);
+	} 
+	//else if (collider.gameObject.tag =="curry"){
+		//Debug.Log ("Haha, you missed my head!");
+	//}
+	
+	}
 
 }
 
